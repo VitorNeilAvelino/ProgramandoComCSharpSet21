32f77cc80bb89bdebc74376e1b49ffd17dcfae0f
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-09-06 19:47:58 -0300

    Do/while no gerador de senha

--- CSharp.Capitulo02.GeradorSenha/Program.cs
+++ CSharp.Capitulo02.GeradorSenha/Program.cs
@@ -6,24 +6,13 @@ namespace CSharp.Capitulo02.GeradorSenha
     {
         static void Main(string[] args)
         {
-            Console.Write("Informe a quantidade de dígitos da senha (entre 4 e 10): ");
+            int quantidadeDigitos;
 
-            var quantidadeDigitos = 0;
-
-            while (quantidadeDigitos == 0)
+            do
             {
+                Console.Write("Informe a quantidade de dígitos da senha (entre 4 e 10): ");
                 quantidadeDigitos = ObterQuantidadeDigitos();
-            }
-
-            //var quantidadeDigitos = Convert.ToInt32(Console.ReadLine());
-
-            //if (quantidadeDigitos < 4 || quantidadeDigitos > 10 || quantidadeDigitos % 2 != 0)
-            if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
-            {
-                Console.WriteLine($"A quantidade de dígitos {quantidadeDigitos} é inválida de acordo com as regras.");
-                Console.ReadKey();
-                return;
-            }
+            } while (quantidadeDigitos == 0);
 
             var senha = string.Empty;
             var randomico = new Random();
@@ -42,7 +31,11 @@ namespace CSharp.Capitulo02.GeradorSenha
         {
             int.TryParse(Console.ReadLine(), out int quantidadeDigitos);
 
-
+            if (quantidadeDigitos is < 4 or > 10 || quantidadeDigitos % 2 != 0)
+            {
+                Console.WriteLine($"A quantidade de dígitos {quantidadeDigitos} é inválida de acordo com as regras.");
+                quantidadeDigitos = 0;
+            }
 
             return quantidadeDigitos;
         }
