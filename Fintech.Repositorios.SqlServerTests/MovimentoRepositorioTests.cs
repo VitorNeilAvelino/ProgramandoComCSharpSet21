af979a814260a6a2e4db24767a0503417684aa0b
Vitor Neil Avelino <avelino.vitor@gmail.com>
2021-09-17 22:49:21 -0300

    Insert na tabela Movimento

---
+++ Fintech.Repositorios.SqlServerTests/MovimentoRepositorioTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fintech.Repositorios.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fintech.Dominio.Entidades;
+
+namespace Fintech.Repositorios.SqlServer.Tests
+{
+    [TestClass()]
+    public class MovimentoRepositorioTests
+    {
+        private readonly MovimentoRepositorio repositorio = new(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Fintech;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        [TestMethod()]
+        public void InserirTest()
+        {
+            var agencia = new Agencia();
+            agencia.Numero = 22;
+
+            var contaFake = new ContaCorrente(agencia, 233, "4");
+            var movimento = new Movimento(50, Operacao.Deposito);
+            movimento.Conta = contaFake;
+
+            repositorio.Inserir(movimento);
+        }
+    }
+}
